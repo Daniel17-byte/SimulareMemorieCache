aa6a425bdfc730197174e886e06d8e964b86b5fc
Daniel Lungu <daniellungu@Daniels-MacBook-Pro.local>
2023-10-15 11:24:38 +0300

    first commit

---
+++ src/main/java/com/example/proiectssc/ProiectSscApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiectssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectSscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectSscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proiectssc/ProiectSscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiectssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectSscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
